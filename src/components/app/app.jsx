1f54abe89bee8e0a25fc070444811766091cf5a3
Nastya N <asrozova@gmail.com>
2023-11-29 01:01:22 +0100

    feat: base layout & adaptive

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,8 @@ import TableBody from "../table-body/table-body";
 import fontStyles from "../../fonts/fonts.module.css";
 
 function App() {
+
+
   return (
     <body className={styles.page}>
       <Header />
@@ -15,9 +17,23 @@ function App() {
           <Filters />
           <Sort />
         </ul>
-        <div className={styles.choose_container}>
-          <label className={`${styles.choose_header} ${fontStyles.light_italic}`} for="choose">-choose</label>
-          <input type="checkbox" name="" id="" value="choose" />
+        <div className={styles.choose_line_container}>
+          <div className={styles.choose_container}>
+            <label
+              className={`${styles.choose_header} ${fontStyles.light_italic}`}
+              for="choose"
+            >
+              -choose
+            </label>
+            <input type="checkbox" name="" id="" value="choose" />
+          </div>
+          <ul className={`${styles.quantity_container} ${fontStyles.light_italic}`}>
+            <li className={styles.quantity}>10</li>
+            <li className={styles.quantity}>20</li>
+            <li className={styles.quantity}>50</li>
+            <li className={styles.quantity}>100</li>
+            <li className={styles.quantity}>all</li>
+          </ul>
         </div>
         <TableBody />
       </main>
--- src/components/post-card/post-card.jsx
+++ src/components/post-card/post-card.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./post-card.module.css";
 import fontStyles from "../../fonts/fonts.module.css";
+import trash from "../../images/trash.svg";
+import comments from "../../images/comment-dots.svg";
+import edit from "../../images/pen-square.svg";
+import starInactive from "../../images/star_inactive.svg";
+import starInactiveHover from "../../images/star_inactive_hover.svg";
+import starActiveHover from "../../images/star_active_hover.svg";
+import starActive from "../../images/star_active.svg";
 
 function PostCard() {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
   return (
     <div className={styles.card_container}>
       <div className={styles.name_container}>
@@ -20,6 +32,33 @@ function PostCard() {
         enim veritatis nihil magnam, aut nam perspiciatis nulla, rem iste dolore
         dolorum!
       </p>
+      {/* <span className={`${fontStyles.bold} ${styles.span_button}`}>
+        Read more
+      </span> */}
+      <ul className={styles.icons_container}>
+        <li>
+          <ul className={styles.main_icons_container}>
+            <li>
+              <img src={comments} alt="comments" className={styles.icon_main} />
+            </li>
+            <li>
+              <img src={edit} alt="edit" className={styles.icon_main} />
+            </li>
+            <li>
+              <img src={trash} alt="trash" className={styles.icon_main} />
+            </li>
+          </ul>
+        </li>
+        <li>
+          <img
+            src={isHovered ? starInactiveHover : starInactive}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+            alt="favorite"
+            className={styles.icon_favorite}
+          />
+        </li>
+      </ul>
     </div>
   );
 }
