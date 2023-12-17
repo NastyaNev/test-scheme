a859efbeb0a5d757cf807f565880155926edef44
Nastya N <asrozova@gmail.com>
2023-12-17 22:13:28 +0100

    first commit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,27 +1,11 @@
 import React from "react";
 import "./app.scss";
-import { Route, Routes } from "react-router";
-import Layout from "../layout/Layout";
-import Control from "../../pages/control/Control";
-import Calibration from "../../pages/calibration/Calibration";
-import WifiList from "../wifi-list/WifiList";
-import ConnectForm from "../connect-form/ConnectForm";
-import NotFound from "../../pages/not-found/NotFound";
-import Wifi from "../../pages/wifi/Wifi";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Control />} />
-        <Route path="calibration" element={<Calibration />} />
-        <Route path="/wifi" element={<Wifi />}>
-          <Route path="/wifi" element={<WifiList />} />
-          <Route path="/wifi/connection-to-/:id" element={<ConnectForm />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <div>
+      <p>hello</p>
+    </div>
   );
 }
 
